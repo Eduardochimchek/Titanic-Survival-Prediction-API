20cd48dacc8db0162b66166f3d9b897bb8c06ee6
Eduardo C. Jeronimo <eduardo.jeronimo@agrosys.com.br>
2024-11-14 11:07:54 -0300

    Ajustes modelo.pkl

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, request, jsonify
 import numpy as np
 import joblib
@@ -7,23 +8,23 @@ from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 
 # Carregar o modelo treinado
-model = joblib.load('model/modelo_titanic.pkl')
+model = joblib.load('model/melhor_modelo_titanic.pkl')
 
 # Definir o mapeamento para 'Sex' e 'Embarked'
 sex_mapping = {"male": 0, "female": 1}
 embarked_mapping = {"C": 0, "Q": 1, "S": 2}
 
-# Carregar o scaler utilizado para a padroniza√ß√£o (assumindo que foi salvo em 'scaler.pkl')
+# Carregar o scaler utilizado para a padronizaÁ„o (assumindo que foi salvo em 'scaler.pkl')
 scaler = joblib.load('model/scaler.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Obter os dados da requisi√ß√£o
+    # Obter os dados da requisiÁ„o
     data = request.get_json()
 
-    # Mapear 'Sex' e 'Embarked' para valores num√©ricos
-    data['Sex'] = sex_mapping.get(data['Sex'], -1)  # Valor -1 se n√£o encontrar
-    data['Embarked'] = embarked_mapping.get(data['Embarked'], -1)  # Valor -1 se n√£o encontrar
+    # Mapear 'Sex' e 'Embarked' para valores numÈricos
+    data['Sex'] = sex_mapping.get(data['Sex'], -1)  # Valor -1 se n„o encontrar
+    data['Embarked'] = embarked_mapping.get(data['Embarked'], -1)  # Valor -1 se n„o encontrar
 
     # Preparar os dados de entrada para o modelo
     input_data = np.array([[data["Pclass"], data["Sex"], data["Age"], data["SibSp"], data["Parch"], data["Fare"], data["Embarked"]]])
@@ -31,10 +32,10 @@ def predict():
     # Escalar os dados
     data_scaled = scaler.transform(input_data)
 
-    # Realizar a predi√ß√£o
+    # Realizar a prediÁ„o
     prediction = model.predict(data_scaled)
 
-    # Retornar a predi√ß√£o como resposta JSON
+    # Retornar a prediÁ„o como resposta JSON
     return jsonify({"prediction": int(prediction[0]), "message": "Previsao realizada com sucesso!"})
 
 if __name__ == '__main__':
